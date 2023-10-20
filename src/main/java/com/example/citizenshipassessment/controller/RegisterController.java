a24e789d2bfef97568bbf114fe76c18ece9e08d0
Kripal Shrestha <kripalshr@gmail.com>
2023-10-20 14:49:10 +0545

    register page added - switches between login & register page

--- src/main/java/com/example/citizenshipassessment/controller/RegisterController.java
+++ src/main/java/com/example/citizenshipassessment/controller/RegisterController.java
@@ -2,33 +2,22 @@ package com.example.citizenshipassessment.controller;
 
 import com.example.citizenshipassessment.MainApp;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginController {
-    @FXML
-    private Label loginVerify;
-
+public class RegisterController {
     private Stage stage;
     private Scene scene;
 
-    public void switchToRegisterPage(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("register-view.fxml"));
+    public void switchToLoginPage(ActionEvent event) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("login-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
-
-
-    @FXML
-   protected void onLoginButtonClick(){
-       loginVerify.setText("Successefully Loggedin");
-   }
 }
