2810b6bf58790ac7534494f580702a092374cba1
Kripal Shrestha <kripalshr@gmail.com>
2023-11-05 16:55:30 +0545

    App COnfigured

--- src/main/java/com/example/citizenshipassessment/AppConfig.java
+++ src/main/java/com/example/citizenshipassessment/AppConfig.java
@@ -3,7 +3,7 @@ package com.example.citizenshipassessment;
 public class AppConfig {
     public static final String DB_URL = "jdbc:mysql://localhost:3306/citizenship_assesment";
     public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "your-password`";
+    public static final String DB_PASSWORD = "creep@lxth@?8";
 }
 
 
