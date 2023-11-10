b9923537df698b0de4ca51541d421d6c6ffc721c
Kripal Shrestha <kripalshr@gmail.com>
2023-11-10 08:27:23 +0545

    username displayed and assesment age verification functionality added

--- src/main/java/com/example/citizenshipassessment/controller/DashboardController.java
+++ src/main/java/com/example/citizenshipassessment/controller/DashboardController.java
@@ -1,16 +1,19 @@
 package com.example.citizenshipassessment.controller;
 
+import com.example.citizenshipassessment.AppConfig;
 import com.example.citizenshipassessment.MainApp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class DashboardController {
 
@@ -23,26 +26,76 @@ public class DashboardController {
     @FXML
     private Button logout;
 
-
-
     public void setLoggedInUsername(String username) {
         loggedInUsername = username;
         userName.setText(loggedInUsername);
     }
 
+    private int calculateAge(Date birthDate) {
+        if (birthDate != null) {
+            long currentTime = System.currentTimeMillis();
+            long birthTime = birthDate.getTime();
+            long millisecondsPerYear = 1000L * 60 * 60 * 24 * 365;
+            return (int) ((currentTime - birthTime) / millisecondsPerYear);
+        }
+        return 0;
+    }
+
+    private boolean isUserAbove18(Date birthDate) {
+        int age = calculateAge(birthDate);
+        return age >= 18;
+    }
+
     public String getLoggedInUsername() {
         return loggedInUsername;
     }
+
     public void handleStartAssessmentButton(ActionEvent event) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("quiz.fxml"));
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(fxmlLoader.load());
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
+        // Fetch user information from the database
+        Date dob = fetchUserDOB(loggedInUsername);
+
+        // Check if the user is above 18
+        if (isUserAbove18(dob)) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("quiz.fxml"));
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(fxmlLoader.load());
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // Show an alert indicating the user needs to be 18 or older
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Age Verification");
+            alert.setHeaderText(null);
+            alert.setContentText("You must be 18 years or older to take this assessment.");
+            alert.showAndWait();
+        }
+    }
+
+    private Date fetchUserDOB(String username) {
+        // Modify these variables with your database connection details
+        String url = AppConfig.DB_URL;
+        String dbUsername = AppConfig.DB_USERNAME;
+        String dbPassword = AppConfig.DB_PASSWORD;
+
+        try (Connection connection = DriverManager.getConnection(url, dbUsername, dbPassword)) {
+            String sql = "SELECT DOB FROM users WHERE username = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, username);
+                ResultSet resultSet = statement.executeQuery();
+
+                if (resultSet.next()) {
+                    return resultSet.getDate("DOB");
+                }
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        return null;
     }
 
     @FXML
